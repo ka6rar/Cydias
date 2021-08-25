ea272d290810c1396ed759bd3073b38b9ae76054
karar <karar@localhost.localdomain>
2021-08-25 14:22:03 -0400

    ssssss

--- resources/views/tools/create.blade.php
+++ resources/views/tools/create.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 @section('content')
-    <form action="{{ route('store') }}" method="post" enctype="multipart/form-data">
+    <form action="{{ route('store') }}" method="POST" enctype="multipart/form-data">
         @csrf
         <input type="file" name="deb">
         <button type="submit">Add</button>
