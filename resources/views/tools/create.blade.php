965f6ed200acabb80d58672376cb7872e4c7a4af
karar <karar@localhost.localdomain>
2021-08-25 14:08:13 -0400

    sssss

--- resources/views/tools/create.blade.php
+++ resources/views/tools/create.blade.php
@@ -3,7 +3,6 @@
     <form action="{{ route('store') }}" method="post" enctype="multipart/form-data">
         @csrf
         <input type="file" name="deb">
-
         <button type="submit">Add</button>
     </form>
 @endsection
